34177e2d841d7ab28a2e2c99e85dbfa74b9c48a7
박성현 <qkr970504@gmail.com>
2023-09-25 09:28:37 +0900

    item1 외부 인스턴스 생성 통제

---
+++ effective-java-part1/src/main/java/me/whiteship/chapter01/item01/Difficulty.java
@@ -0,0 +1,4 @@
+package me.whiteship.chapter01.item01;
+
+public class Difficulty {
+}
---
+++ effective-java-part1/src/main/java/me/whiteship/chapter01/item01/Settings.java
@@ -0,0 +1,22 @@
+package me.whiteship.chapter01.item01;
+
+public class Settings {
+
+    private boolean useAutoSteering;
+
+    private boolean useABS;
+
+    private Difficulty difficulty;
+
+    // 인스턴스 생성을 통제하고 싶다면 private 선언
+    // 정적 팩토리 메서드 제공
+    // 객체 생성을 스스로 통제 -> 외부에서 Settings 객체 생성 못함
+    // 오직 팩토리 메서드로만 객체 생성.
+    private Settings() {}
+
+    private static final Settings SETTINGS = new Settings();
+
+    public static Settings newInstance() {
+        return SETTINGS;
+    }
+}
