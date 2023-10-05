a2ab49b900d55d43b211bf07f8f9c37930d7ca05
박성현 <qkr970504@gmail.com>
2023-10-05 09:35:30 +0900

    Item15 접근 제한자 사용원칙 - 중첩클래스 ~ing

---
+++ effective-java-part2/src/main/java/org/example/chapter04/item15/member/Member.java
@@ -0,0 +1,4 @@
+package org.example.chapter04.item15.member;
+
+public class Member {
+}
---
+++ effective-java-part2/src/main/java/org/example/chapter04/item15/member/MemberService.java
@@ -0,0 +1,4 @@
+package org.example.chapter04.item15.member;
+
+public interface MemberService { // 톱레벨 클래스는 public 아니면 package-private
+}
