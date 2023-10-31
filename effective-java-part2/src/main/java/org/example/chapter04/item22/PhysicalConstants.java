86554c88792d1c3eeb19379fe17602a3aeab132f
박성현 <qkr970504@gmail.com>
2023-10-31 09:31:09 +0900

    Item22 정규화된 이름(qualified name)

--- effective-java-part2/src/main/java/org/example/chapter04/item22/PhysicalConstants.java
+++ effective-java-part2/src/main/java/org/example/chapter04/item22/PhysicalConstants.java
@@ -1,13 +1,13 @@
 package org.example.chapter04.item22;
 
-public class PhysicalConstants {
+public class PhysicalConstants implements PhysicalConstantsInterface{
 
-    private PhysicalConstants() {}
+    public static void main(String[] args) {
+        System.out.println(PhysicalConstantsInterface.AVOGADROS_NUMBER);
+        System.out.println(AVOGADROS_NUMBER);
+    }
 
-    // 아보가드로 수 (1/몰)
-    static final double AVOGADROS_NUMBER   = 6.022_140_857e23;
-    // 볼츠만 상수 (J/K)
-    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
-    // 전자 질량 (kg)
-    static final double ELECTRON_MASS      = 9.109_383_56e-31;
+//    static final double AVOGADROS_NUMBER   = 6.022_140_857e23;
+//    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+//    static final double ELECTRON_MASS      = 9.109_383_56e-31;
 }
---
+++ effective-java-part2/src/main/java/org/example/chapter04/item22/PhysicalConstantsInterface.java
@@ -0,0 +1,7 @@
+package org.example.chapter04.item22;
+
+public interface PhysicalConstantsInterface {
+    static final double AVOGADROS_NUMBER   = 6.022_140_857e23;
+    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+    static final double ELECTRON_MASS      = 9.109_383_56e-31;
+}
