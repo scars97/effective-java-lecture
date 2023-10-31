1f7c6a68257ee5baec79aa798555a11e2ae66bf3
박성현 <qkr970504@gmail.com>
2023-10-31 09:13:36 +0900

    Item22 정리

---
+++ effective-java-part2/src/main/java/org/example/chapter04/item22/PhysicalConstants.java
@@ -0,0 +1,13 @@
+package org.example.chapter04.item22;
+
+public class PhysicalConstants {
+
+    private PhysicalConstants() {}
+
+    // 아보가드로 수 (1/몰)
+    static final double AVOGADROS_NUMBER   = 6.022_140_857e23;
+    // 볼츠만 상수 (J/K)
+    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+    // 전자 질량 (kg)
+    static final double ELECTRON_MASS      = 9.109_383_56e-31;
+}
---
+++ effective-java-part2/src/main/java/org/example/chapter04/item22/Test.java
@@ -0,0 +1,9 @@
+package org.example.chapter04.item22;
+
+import static org.example.chapter04.item22.PhysicalConstants.*;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(AVOGADROS_NUMBER);
+    }
+}
