5c24f77f60c6ba10122b9621473a14ca03035a00
박성현 <qkr970504@gmail.com>
2023-11-06 12:54:14 +0900

    Item24 정적 클래스, 비정적 클래스

---
+++ effective-java-part2/src/main/java/org/example/chapter04/item24/StaticApp.java
@@ -0,0 +1,14 @@
+package org.example.chapter04.item24;
+
+public class StaticApp {
+
+    public static void main(String[] args) {
+        StaticMemberClass.ExStaticClass ex = new StaticMemberClass.ExStaticClass();
+
+
+
+//        StaticMemberClass ex = new StaticMemberClass();
+//
+//        StaticMemberClass.ExStaticClass exStaticClass = ex.new ExStaticClass();
+    }
+}
