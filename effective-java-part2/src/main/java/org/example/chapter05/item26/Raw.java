41bb95d3e22f1f7366e4826fd581c1a46e4bba57
박성현 <qkr970504@gmail.com>
2023-11-08 09:13:47 +0900

    Item26 제네릭 - 로 타입

---
+++ effective-java-part2/src/main/java/org/example/chapter05/item26/Raw.java
@@ -0,0 +1,16 @@
+package org.example.chapter05.item26;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Raw {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        unsafeAdd(strings, Integer.valueOf(42));
+        String s = strings.get(0); // 컴파일러가 자동으로 형변환 코드를 넣어준다.
+    }
+
+    private static void unsafeAdd(List<Object> list, Object o) {
+        list.add(o);
+    }
+}
