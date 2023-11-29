f131bf650f7361e7fafcb13e05e8f223d45a739d
박성현 <qkr970504@gmail.com>
2023-11-29 22:43:49 +0900

    Item33 타입 안전 이종 컨테이너 정리

--- effective-java-part2/src/main/java/org/example/Main.java
+++ effective-java-part2/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> stringList = new ArrayList<>();
+        stringList.add(4);
+        test(stringList);
+    }
+
+    public static void test(List<?> lists) {
+        String genericString = lists.getClass().toGenericString();
+        System.out.println("genericString = " + genericString);
+        System.out.println(lists.get(0));
     }
 }
